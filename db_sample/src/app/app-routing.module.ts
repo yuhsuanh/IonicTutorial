83cded429a3de72963ea1d37c88dca411eee9a41
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-04-14 17:41:44 -0400

    Step 1. Create page, set up router and create dump sql file

--- db_sample/src/app/app-routing.module.ts
+++ db_sample/src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'developer', pathMatch: 'full' },
+  {
+    path: 'developer',
+    loadChildren: () => import('./pages/developer/developer.module').then( m => m.DeveloperPageModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./pages/product/product.module').then( m => m.ProductPageModule)
+  },
 ];
 
 @NgModule({
