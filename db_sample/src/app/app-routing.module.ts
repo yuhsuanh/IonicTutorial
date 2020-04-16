1e7016c1093cd8be41f6fe01f657cd2d20aabd8d
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-04-15 21:03:11 -0400

    Step 1.

--- db_sample/src/app/app-routing.module.ts
+++ db_sample/src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'developer', pathMatch: 'full' },
+  {
+    path: 'developer',
+    loadChildren: () => import('./pages/developer/developer.module').then( m => m.DeveloperPageModule)
+  },
+  {
+    path: 'skill',
+    loadChildren: () => import('./pages/skill/skill.module').then( m => m.SkillPageModule)
+  },
 ];
 
 @NgModule({
